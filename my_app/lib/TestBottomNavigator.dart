12e5160f5f801da65fa43625b8526d199b73c3d1
min <ken1336@naver.com>
2020-08-29 05:45:16 +0900

    Add page swipe to TestBottomNavigator

--- my_app/lib/TestBottomNavigator.dart
+++ my_app/lib/TestBottomNavigator.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-
-class TestRouter extends StatelessWidget{
+class TestRouter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,44 +8,127 @@ class TestRouter extends StatelessWidget{
         title: Text("Hello this is test Router"),
       ),
       body: TestBottomNavigator(),
-      
     );
     // TODO: implement build
-    
-  } 
+  }
 }
 
-class TestBottomNavigator extends StatefulWidget{
+class TestBottomNavigator extends StatefulWidget {
   @override
   _TestBottomNavigatorState createState() => _TestBottomNavigatorState();
-
-
 }
 
-class _TestBottomNavigatorState extends State<TestBottomNavigator>{
-
+class _TestBottomNavigatorState extends State<TestBottomNavigator> {
   var _currentIndex = 0;
+  final List<Widget> _childWidget = [
+    _HomeWidget(),
+    _AppWidget(),
+    _PersonWidget()
+  ];
+
+  var _pageController = PageController(initialPage: 0);
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-            body: Text("hi"),
-            bottomNavigationBar: BottomNavigationBar(
-                type: BottomNavigationBarType.fixed,
-                currentIndex: _currentIndex,
-                items: _TestBottomNavigationBarItem.toList(),
-            onTap: (index) =>setState((){
-              print(index);
-              _currentIndex = index;
-            })
-    ));
+        body: PageView(
+          controller: _pageController,
+          onPageChanged: (newPage){
+          setState((){
+            this._currentIndex=newPage;
+          });
+        },
+        children: _childWidget,
+    
+        ),
+        
+        bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            currentIndex: _currentIndex,
+            items: _TestBottomNavigationBarItem.toList(),
+            onTap: (index) => setState(() {
+                  print(index);
+                  _pageController.animateToPage(index, duration: Duration(milliseconds: 500), curve: Curves.easeInOut);
+                  _currentIndex = index;
+                })));
   }
-
 }
 
 const List<BottomNavigationBarItem> _TestBottomNavigationBarItem = [
-  BottomNavigationBarItem(icon: Icon(Icons.home),title: Text('Home')),
-  BottomNavigationBarItem(icon: Icon(Icons.apps),title: Text('apps')),
-  BottomNavigationBarItem(icon: Icon(Icons.person),title: Text('person'))
-  ];
+  BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+  BottomNavigationBarItem(icon: Icon(Icons.apps), title: Text('apps')),
+  BottomNavigationBarItem(icon: Icon(Icons.person), title: Text('person'))
+];
 
+class _HomeWidget extends StatefulWidget {
+  @override
+  _HomeWidgetState createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<_HomeWidget> with AutomaticKeepAliveClientMixin{
+  var _count = 0;
+  
+  @override
+  bool get wantKeepAlive => true;
+  
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    print(_count);
+    return Scaffold(
+        body: Text('Home count: $_count'),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.plus_one),
+          onPressed: () => setState(() {
+            _count += 1;
+          }),
+        ));
+  }
+
+
+}
+
+class _AppWidget extends StatefulWidget {
+  @override
+  _AppWidgetState createState() => _AppWidgetState();
+}
+
+class _AppWidgetState extends State<_AppWidget> with AutomaticKeepAliveClientMixin{
+  var _count = 0;
+  @override
+  bool get wantKeepAlive => true;
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return Scaffold(
+        body: Text('app count: $_count'),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.plus_one),
+          onPressed: () => setState(() {
+            _count += 1;
+          }),
+        ));
+  }
+}
+
+class _PersonWidget extends StatefulWidget {
+  @override
+  _PersonWidgetState createState() => _PersonWidgetState();
+}
+
+class _PersonWidgetState extends State<_PersonWidget> with AutomaticKeepAliveClientMixin{
+  var _count = 0;
+  @override
+  bool get wantKeepAlive => true;
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return Scaffold(
+        body: Text('Person count: $_count'),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.plus_one),
+          onPressed: () => setState(() {
+            _count += 1;
+          }),
+        ));
+  }
+}
